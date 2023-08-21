716604aeec7f72e22bec07e79311742824382dab
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-21 20:32:22 +0200

    feat: Добавил каркас
    Использовал один из готовых каркасов Vuetify

---
+++ src/layouts/constrained/LayoutMain.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import { navElements } from "@/menu-elements";
+import DummyComponent from "@/components/DummyComponent.vue";
+const links = ["Dashboard", "Messages", "Profile", "Updates"];
+</script>
+
+<template>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
+
+        <!-- Навигация из файла App.vue -->
+        <nav>
+          <RouterLink
+            v-for="(navElement, i) in navElements"
+            :key="i"
+            :to="navElement.path"
+          >{{ navElement.nameMenu }}</RouterLink>
+        </nav>
+
+        <v-btn v-for="link in links" :key="link" variant="text">
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item v-for="n in 5" :key="n" link>
+                  <v-list-item-title>List Item {{ n }}</v-list-item-title>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item link color="grey-lighten-4">
+                  <v-list-item-title>Refresh</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <!-- Подключение страниц маршрутизатором из файла App.vue -->
+              <router-view />
+
+              <!-- Баг на GitHub Pages. Обходим его, добавляя этот компонент. -->
+              <dummy-component></dummy-component>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
