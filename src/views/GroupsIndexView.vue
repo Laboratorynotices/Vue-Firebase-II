490ff09988e6ef1572d60323067e48f28c8a0762
Laboratorynotices <muenchen.webas@gmail.com>
2023-08-23 00:14:07 +0200

    feat: Перенос запроса кода в GroupsIndexView

--- src/views/GroupsIndexView.vue
+++ src/views/GroupsIndexView.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase/config";
+onMounted(async () => {
+  const querySnapshot = await getDocs(collection(db, "groups"));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+});
+</script>
+
 <template>
   <v-card width="400" class="mx-auto">
     <v-card-item>
